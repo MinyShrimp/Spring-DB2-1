2835bd88835a0b12157d3554b3ed0dcb45346b4b
MinyShrimp <ksk7584@gmail.com>
2023-02-26 21:55:04 +0900

    2강: 테스트 - 임베디드 모드 DB

--- src/main/java/hello/springdb2/SpringDb2Application.java
+++ src/main/java/hello/springdb2/SpringDb2Application.java
@@ -7,8 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-//@Import(JdbcTemplateV2Config.class)
+import javax.sql.DataSource;
+
 @Import(JdbcTemplateV3Config.class)
 @SpringBootApplication(scanBasePackages = "hello.springdb2.controller")
 public class SpringDb2Application {
@@ -23,5 +25,16 @@ public class SpringDb2Application {
             ItemRepository itemRepository
     ) {
         return new TestDataInit(itemRepository);
+    }
+
+    @Bean
+    @Profile("test")
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.h2.Driver");
+        dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+        dataSource.setUsername("sa");
+        dataSource.setPassword("");
+        return dataSource;
     }
 }
